a5def9cf0f60b3ec73e0df69fdb44c9ba96db608
Gary Muir <garymuir17@gmail.com>
2018-05-29 17:02:11 +0100

    River class created, and get_river_name test passed

---
+++ river.rb
@@ -0,0 +1,22 @@
+class River
+
+  attr_reader(:name)
+
+  def initialize(name, fish_number)
+    @name = name
+    @fish_number = fish_number
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -5,11 +5,15 @@ require_relative("../fish.rb")
 class RiverTest < MiniTest::Test
 
   def setup
-    @river1 = River.new("Isonzo", [@fish1, @fish2, @fish3])
+    fish1 = Fish.new("Graham")
+    fish2 = Fish.new("Billy")
+    fish3 = Fish.new("Craig")
+
+    @river1 = River.new("Isonzo", [fish1, fish2, fish3])
   end
 
   def test_get_river_name
-    assert_equal("Isonzi", @river1.name())
+    assert_equal("Isonzo", @river1.name())
   end
 
 
