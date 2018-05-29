e63ba094157380a1a8b1b2e04c418caa7e4ba1f2
Gary Muir <garymuir17@gmail.com>
2018-05-29 16:50:30 +0100

    fish_spec.rb setup done

---
+++ specs/fish_spec.rb
@@ -0,0 +1,25 @@
+require("minitest/autorun")
+require_relative("../fish.rb")
+
+class FishTest < MiniTest::Test
+
+  def setup
+    @fish1 = Fish.new("Graham")
+    @fish2 = Fish.new("Billy")
+    @fish3 = Fish.new("Craig")
+  end
+
+  def test_get_fish_name
+    assert_equal("Graham", @fish1.name())
+  end
+
+
+
+
+
+
+
+
+
+
+end
